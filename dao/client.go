f70f85ca90118cb1f04a2c137c2862164726f7a3
cqhasy <3181940782@qq.com>
2025-06-25 11:14:32 +0800

    webhook make

--- dao/client.go
+++ dao/client.go
@@ -1,4 +1,4 @@
-package client
+package dao
 
 import (
 	"gorm.io/driver/mysql"
--- demo/demo.go
+++ demo/demo.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gorm.io/gorm"
-	"muxisdk/client"
+	"muxisdk/dao"
 )
 
 type User struct {
@@ -16,7 +16,7 @@ type User struct {
 
 func main() {
 	//创建Client并初始化表
-	c := client.NewOrmClient("root:chenhaoqi318912@tcp(60.205.12.92:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local")
+	c := dao.NewOrmClient("root:chenhaoqi318912@tcp(60.205.12.92:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local")
 	c.InitTable(&User{})
 	var co = context.Background()
 	var user = User{Name: "c", Email: "chenhaoqi318912@qq.com", Avatar: 12}
@@ -27,18 +27,18 @@ func main() {
 	}
 	//获取数据，需要什么数据就传什么数据类型
 	var user1 User
-	c.Get(co, &user1, client.Key{"Avatar", 12})
+	c.Get(co, &user1, dao.Key{"Avatar", 12})
 	err = c.Update(co, &User{
 		Name:   "",
 		Email:  "",
 		Avatar: 122,
-	}, client.Key{"Name", "c"})
+	}, dao.Key{"Name", "c"})
 	if err != nil {
 		fmt.Println(err)
 	}
-	c.Get(co, &user1, client.Key{"Avatar", 122})
+	c.Get(co, &user1, dao.Key{"Avatar", 122})
 	fmt.Println(user1)
-	err = c.Delete(co, &user1, client.Key{"Avatar", 122})
+	err = c.Delete(co, &user1, dao.Key{"Avatar", 122})
 	if err != nil {
 		fmt.Println(err)
 	}
